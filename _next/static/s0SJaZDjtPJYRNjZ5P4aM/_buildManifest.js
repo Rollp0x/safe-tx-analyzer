a8d016c3cde2eb7f6934d2a29ca82f5eba7a324e
Rollp0x <zkrollp@gmail.com>
2024-12-10 12:49:33 +0000

    Deploying to gh-pages from @ Rollp0x/safe-tx-analyzer@7de62345fcc0eb3887815692e2f021fa42cc06cd 🚀

---
+++ _next/static/s0SJaZDjtPJYRNjZ5P4aM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-9cefe7a3122b7c6c.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
