5488897835d59bdda25a3aa63d9c94b8bad89fba
MetaZeroFairyProof <zero@fairyproof.com>
2024-12-10 22:44:20 +0800

    添加assetPrefix尝试修复资源404

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
     },
     output: 'export',  // 静态导出
     basePath: '/safe-tx-analyzer',  // 添加这行，设置基础路径
+    assetPrefix: '/safe-tx-analyzer/',  // 添加这行
     images: {
         unoptimized: true, // GitHub Pages 不支持 Next.js 的图片优化
     },
