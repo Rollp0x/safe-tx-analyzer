7de62345fcc0eb3887815692e2f021fa42cc06cd
MetaZeroFairyProof <zero@fairyproof.com>
2024-12-10 20:47:45 +0800

    修改编译后输出的位置以放置页面

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
         ignoreDuringBuilds: true,
     },
     output: 'export',  // 静态导出
-    distDir: 'build', // 将 .next 改为 build
     basePath: '/safe-tx-analyzer',  // 添加这行，设置基础路径
     images: {
         unoptimized: true, // GitHub Pages 不支持 Next.js 的图片优化
