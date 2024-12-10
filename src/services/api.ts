5f342da85f7156094cccdf917ee0ced9c6788423
MetaZeroFairyProof <zero@fairyproof.com>
2024-12-10 15:28:19 +0800

    增加代理以应对cors，修复签名拼接时的问题

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 
 import instance from './axios'; // 我们配置的 axios 实例
-import axios, { AxiosResponse } from 'axios';       // 原生 axios
+import axios from 'axios';       // 原生 axios
 import { TraceRequest, SafeTraceRequest, TraceResponse, SignedSafeTx } from '@/types';
-
-const safe_url = process.env.NEXT_PUBLIC_BITLAYER_SAFE_URL;
 
 export const api = {
   // 交易追踪
@@ -14,10 +12,8 @@ export const api = {
   traceSafe: (request: SafeTraceRequest) =>
     instance.post<TraceResponse>('/trace/safe', request),
 
-
   // Safe 交易提交
-  proposeSafeTx: (safeAddress: string, signedTx: SignedSafeTx) : Promise<AxiosResponse<void>> => 
-    axios.post<void>(`${safe_url}/${safeAddress}/propose`, signedTx),
+  proposeSafeTx: (safeAddress: string, signedTx: SignedSafeTx) => 
+    axios.post<void>('/api/safe/propose', { safeAddress, signedTx }),
 };
-  
   
