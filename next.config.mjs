f42c9addb687a310b51347fd9933fda6846045f4
MetaZeroFairyProof <zero@fairyproof.com>
2024-12-10 20:20:50 +0800

    修改部署配置

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,7 @@ const nextConfig = {
     },
     output: 'export',  // 静态导出
     distDir: 'build', // 将 .next 改为 build
-    basePath: '/analysis',  // 添加这行，设置基础路径
-    assetPrefix: '/analysis/',  // 添加这行，设置资源前缀
+    basePath: '/safe-tx-analyzer',  // 添加这行，设置基础路径
     images: {
         unoptimized: true, // GitHub Pages 不支持 Next.js 的图片优化
     },
