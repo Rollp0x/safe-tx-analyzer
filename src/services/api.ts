fa733a3727c414c4d8429546da7274c62c42e97d
MetaZeroFairyProof <zero@fairyproof.com>
2024-12-23 09:54:50 +0800

    更改了api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,20 @@
 
 import instance from './axios'; // 我们配置的 axios 实例
-import axios from 'axios';       // 原生 axios
 import { TraceRequest, SafeTraceRequest, TraceResponse, SignedSafeTx } from '@/types';
 
 export const api = {
   // 交易追踪
   trace: (request: TraceRequest) =>
-    instance.post<TraceResponse>('/trace', request),
+    instance.post<TraceResponse>('/api/decode/internal/trace', request),
 
   // Safe交易追踪
   traceSafe: (request: SafeTraceRequest) =>
-    instance.post<TraceResponse>('/trace/safe', request),
+    instance.post<TraceResponse>('/api/decode/internal/trace/safe', request),
 
   // Safe 交易提交
-  proposeSafeTx: (safeAddress: string, signedTx: SignedSafeTx) => 
-    axios.post<void>('/api/safe/propose', { safeAddress, signedTx }),
+  proposeSafeTx: (safeAddress: string, signedTx: SignedSafeTx) => {
+    let url = `/safe/${safeAddress}/propose`;
+    instance.post<void>(url, signedTx);
+  },
 };
   
