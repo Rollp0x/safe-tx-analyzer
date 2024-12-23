20c841e8c4822558b43cc4004547f9931dc95626
MetaZeroFairyProof <zero@fairyproof.com>
2024-12-23 10:16:17 +0800

    修改api路径

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
         ignoreDuringBuilds: true,
     },
     output: 'export',  // 静态导出
+    distDir: 'build', // 将 .next 改为 build  //拖管到github 需要,否则不识别_next目录
     basePath: '/safe-tx-analyzer',  // 添加这行，设置基础路径
-    assetPrefix: '/safe-tx-analyzer/',  // 添加这行
     images: {
         unoptimized: true, // GitHub Pages 不支持 Next.js 的图片优化
     },
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,18 @@
 import instance from './axios'; // 我们配置的 axios 实例
 import { SafeTraceRequest,TraceResponse,LoginRequest, LoginResponse, SignedSafeTx } from '../types';
-import axios from 'axios'
 
 export const api = {
   // 登录
   login: (request: LoginRequest) =>
-    instance.post<LoginResponse>('/login', request),
+    instance.post<LoginResponse>('/api/decode/external/login', request),
 
   // Safe交易追踪
   traceSafe: (request: SafeTraceRequest) =>
-    instance.post<TraceResponse>('/auth/trace/safe', request),
+    instance.post<TraceResponse>('/api/decode/external/trace/safe', request),
   
   // Safe 交易提交
   proposeSafeTx: (safeAddress: string, signedTx: SignedSafeTx) => 
-    axios.post<void>('/api/safe/propose', { safeAddress, signedTx }),
+    instance.post<void>(`/safe/${safeAddress}/propose`, signedTx),
 
 };
   
